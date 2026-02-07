7aa40c4b513817784bbd6a8a3620869c260e0ff1
Hussain <hussainsiyahiwala4@gmail.com>
2026-02-07 17:08:05 +0530

    coding part done

--- code.c
+++ code.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 
+struct Laptop {
+    int id;
+    char name[50];
+    int price;
+    int brand;
+    int usage;   // THE LOGIC FILTER: 1=Gaming, 2=Student, 3=Coding
+    int ram;     // FOR DISPLAY
+    int storage; // FOR DISPLAY
+};
+
+struct Phone {
+    int id;
+    char name[50];
+    int price;
+    int brand;
+    int os;
+    int usage;   // THE LOGIC FILTER: 1=Camera, 2=Battery, 3=Performance
+    int ram;     // FOR DISPLAY
+    int storage; // FOR DISPLAY
+    int camera;  // FOR DISPLAY
+    int battery; // FOR DISPLAY
+};
+
 void lst_of_all(struct Laptop l[], struct Phone p[]);
 void recommendLaptop(struct Laptop db[]);
 void recommendPhone(struct Phone db[]);
 void printLaptopRow(struct Laptop l);
 void printPhoneRow(struct Phone p);
 
-
 int main() {
-    int lap_budget, lap_usage, lap_ram_str, laptop_lst[20], phone_lst[20],ch; 
+    struct Laptop laptopDB[20]; 
+    struct Phone phoneDB[20];
+    int ch; 
 
+    lst_of_all(laptopDB, phoneDB);
     do {
     printf("\n--- MAIN MENU ---\n");
     printf("1. Find a Laptop");
@@ -39,6 +64,7 @@ int main() {
 
 } while (ch != 3);
 
+    return 0;
 }
 /* 1. Gaming
 2. student/office
@@ -93,40 +119,40 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     l[5].id = 6;
     strcpy(l[5].name, "Samsung Galaxy Book4");
     l[5].price = 54000;
-    l[5].brand = 6;
-    l[5].usage = 3;
+    l[5].brand = 6;  // SAMSUNG = 6
+    l[5].usage = 3;  // 3 = programming
     l[5].ram = 16;
     l[5].storage = 512;
 
     l[6].id = 7;
     strcpy(l[6].name, "Acer Swift Go 14");
     l[6].price = 79000;
-    l[6].brand = 5;
-    l[6].usage = 3;
+    l[6].brand = 5; // ACER = 5
+    l[6].usage = 3; // 3 = programming
     l[6].ram = 16;
     l[6].storage = 1000;
 
     l[7].id = 8;
     strcpy(l[7].name, "Lenovo ThinkPad E14");
     l[7].price = 60000;
-    l[7].brand = 3;
-    l[7].usage = 3;
+    l[7].brand = 3; // LENOVO = 3
+    l[7].usage = 3; // 3 = programming
     l[7].ram = 16;
     l[7].storage = 512;
 
     l[8].id = 9;
     strcpy(l[8].name, "HP Pavilion 14");
     l[8].price = 60000;
-    l[8].brand = 2;
-    l[8].usage = 3;
+    l[8].brand = 2; // HP = 2
+    l[8].usage = 3; // 3 = programming
     l[8].ram = 16;
     l[8].storage = 512;
 
     l[9].id = 10;
     strcpy(l[9].name, "ASUS Vivobook S");
     l[9].price = 56000;
-    l[9].brand = 1;
-    l[9].usage = 3;
+    l[9].brand = 1; // ASUS = 1
+    l[9].usage = 3; // 3 = programming
     l[9].ram = 16;
     l[9].storage = 512;
 
@@ -136,47 +162,44 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     l[10].id = 11;
     strcpy(l[10].name, "Lenovo LOQ 15");
     l[10].price = 85000;
-    l[10].brand = 3;
-    l[10].usage = 1;
+    l[10].brand = 3; // LENOVO = 3
+    l[10].usage = 1; // 1 = gaming
     l[10].ram = 16;
     l[10].storage = 512;
 
     l[11].id = 12;
     strcpy(l[11].name, "ASUS TUF F16");
     l[11].price = 97000;
-    l[11].brand = 1;
-    l[11].usage = 1;
+    l[11].brand = 1;   // ASUS = 1
+    l[11].usage = 1;  // 1 = gaming
     l[11].ram = 16;
     l[11].storage = 512;
 
     l[12].id = 13;
     strcpy(l[12].name, "MSI Katana A15");
     l[12].price = 97000;
-    l[12].brand = 7;
-    l[12].usage = 1;
+    l[12].brand = 7;   // MSI = 7
+    l[12].usage = 1; // 1 = gaming
     l[12].ram = 16;
     l[12].storage = 512;
 
     l[13].id = 14;
     strcpy(l[13].name, "Acer Nitro 16");
     l[13].price = 80000;
-    l[13].brand = 5;
-    l[13].usage = 1;
+    l[13].brand = 5; // ACER = 5
+    l[13].usage = 1; // 1 = gaming
     l[13].ram = 16;
     l[13].storage = 512;
 
     l[14].id = 15;
     strcpy(l[14].name, "Acer Predator Helios Neo 16");
     l[14].price = 155000;
-    l[14].brand = 5;
-    l[14].usage = 1;
+    l[14].brand = 5; // ACER = 5
+    l[14].usage = 1; // 1 = gaming
     l[14].ram = 32;
     l[14].storage = 1000;
-}
 
-
-    
-    // ===== BUDGET PHONES (UNDER 20K) =====
+// ===== BUDGET PHONES (UNDER 20K) =====
 
     p[0].id = 1;
     strcpy(p[0].name, "Poco M7 Pro 5G");
@@ -187,7 +210,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[0].camera = 50;
     p[0].storage = 128;
     p[0].battery = 5110;
-    p[0].usage = 3;        // 3=Basic/ Budget users
+    p[0].usage = 1;
 
     p[1].id = 2;
     strcpy(p[1].name, "Realme Narzo 80 Lite 5G");
@@ -198,7 +221,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[1].camera = 50;
     p[1].storage = 128;
     p[1].battery = 5000;
-    p[1].usage = 3;        // 3=Basic/ Budget users
+    p[1].usage = 2;        // 3=Basic/ Budget users
 
     p[2].id = 3;
     strcpy(p[2].name, "Redmi 15C 5G");
@@ -220,7 +243,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[3].camera = 50;
     p[3].storage = 128;
     p[3].battery = 6000;
-    p[3].usage = 1;        // 1=Best for most users
+    p[3].usage = 2;        
 
     p[4].id = 5;
     strcpy(p[4].name, "Motorola G64 5G");
@@ -231,7 +254,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[4].camera = 50;
     p[4].storage = 64;
     p[4].battery = 6000;
-    p[4].usage = 1;        // 1=Best for most users
+    p[4].usage = 2;        
 
 
     // ===== MID RANGE PHONES (30K-70K) =====
@@ -245,7 +268,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[5].camera = 50;
     p[5].storage = 256;
     p[5].battery = 4000;
-    p[5].usage = 1;        // 1=Best for most users
+    p[5].usage = 1;        
 
     p[6].id = 7;
     strcpy(p[6].name, "Oppo Reno15 Pro 5G");
@@ -256,7 +279,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[6].camera = 200;
     p[6].storage = 256;
     p[6].battery = 5000;
-    p[6].usage = 2;        // 2=Power/Premium users
+    p[6].usage = 3;        
 
     p[7].id = 8;
     strcpy(p[7].name, "iPhone 15");
@@ -267,7 +290,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[7].camera = 48;
     p[7].storage = 128;
     p[7].battery = 3279;
-    p[7].usage = 2;        // 2=Power/Premium users
+    p[7].usage = 1;        
 
     p[8].id = 9;
     strcpy(p[8].name, "Realme 16 Pro 5G");
@@ -278,7 +301,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[8].camera = 200;
     p[8].storage = 128;
     p[8].battery = 7000;
-    p[8].usage = 1;        // 1=Best for most users
+    p[8].usage = 2;        
 
     p[9].id = 10;
     strcpy(p[9].name, "OnePlus Nord 5 5G");
@@ -289,7 +312,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[9].camera = 50;
     p[9].storage = 256;
     p[9].battery = 6800;
-    p[9].usage = 1;        // 1=Best for most users
+    p[9].usage = 2;        
 
     // ===== HIGH RANGE PHONES ( ABOVE 70K) =====
 
@@ -302,7 +325,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[10].camera = 48;
     p[10].storage = 256;
     p[10].battery = 3988;
-    p[10].usage = 2;        // 2=Power/Premium users
+    p[10].usage = 1;        
 
     p[11].id = 12;
     strcpy(p[11].name, "Samsung Galaxy S25 Ultra");
@@ -313,7 +336,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[11].camera = 200;
     p[11].storage = 256;
     p[11].battery = 5000;
-    p[11].usage = 2;        // 2=Power/Premium users
+    p[11].usage = 1;        
 
     p[12].id = 13;
     strcpy(p[12].name, "Google Pixel 10 Pro");
@@ -324,7 +347,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[12].camera = 50;
     p[12].storage = 256;
     p[12].battery = 4870;
-    p[12].usage = 2;        // 2=Power/Premium users
+    p[12].usage = 1;        
 
     p[13].id = 14;
     strcpy(p[13].name, "Xiaomi 15 Ultra");
@@ -335,7 +358,7 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[13].camera = 50;
     p[13].storage = 512;
     p[13].battery = 5410;
-    p[13].usage = 2;        // 2=Power/Premium users
+    p[13].usage = 3;        
 
     p[14].id = 15;
     strcpy(p[14].name, "OnePlus 12 Pro ");
@@ -346,31 +369,60 @@ void lst_of_all(struct Laptop l[], struct Phone p[]) {
     p[14].camera = 50;
     p[14].storage = 512;
     p[14].battery = 5500;
-    p[14].usage = 2;        // 2=Power/Premium users
+    p[14].usage = 3;        
+}
+//  1. Best Camera Quality \n2. Longest Battery Life \n3. High Performance / Gaming
+void recommendLaptop(struct Laptop db[]) {
+    int i, l_budget, l_usage, count = 0;
+        printf("\nWhat's the budget for your laptop? (In rupees): ");
+        scanf("%d", &l_budget);
+        printf("\nChoose your primary usage: 1. Gaming, 2. Student/office work/daily life, 3. Programming, Enter choice (0 for All): ");
+        scanf("%d", &l_usage);
+            for (i = 0; i < 20; i++) {
+                if (db[i].id == 0) {
+                    continue; // it was giving weird output, so I added this to avoid the empty structs in the array
+                }
+                if (db[i].price <= l_budget && (db[i].usage == l_usage || l_usage == 0)) {
+                    printLaptopRow(db[i]);
+                    count++;
+                }
+                }
+            if (count == 0) {
+                printf("Sorry, we coudn't find your desired laptop. Change the filters and try again :)");
+            }
+                    
+}
+
+void printLaptopRow(struct Laptop l) {
+    printf("\n1. Model Name - %s \n2. Price - %d \n3. RAM - %d \n4. Storage - %d \n \n", l.name, l.price, l.ram, l.storage);
 
 }
 
-void recommendLaptop(struct Laptop db[]) {
-    int lap_budget, lap_usage, lap_ram_str, laptop_lst[20], count =
 
+void recommendPhone(struct Phone db[]) {
+    int i, p_budget, p_usage, os_choice, count = 0;
+        printf("\nWhat's the budget for your phone? (In rupees): ");
+        scanf("%d", &p_budget);
+        printf("\nWhat is your top priority? \n1. Best Camera Quality \n2. Longest Battery Life \n3. High Performance / Gaming \nEnter choice (0 for All): ");
+        scanf("%d", &p_usage);
+        printf("\nDo you want an \n1. Andriod \n2. Ios \nEnter choice (0 for All): ");
+        scanf("%d", &os_choice);
+            for (i = 0; i < 20; i++) {
+                if (db[i].id == 0) {
+                    continue; // it was giving weird output, so I added this to avoid the empty structs in the array
+                }
+                if (db[i].price <= p_budget && (db[i].usage == p_usage || p_usage == 0) && (db[i].os == os_choice || os_choice == 0)) {
+                    printPhoneRow(db[i]);
+                    count++;
+                }
+                }
+            if (count == 0) {
+                printf("Sorry, we coudn't find your desired Phone. Change the filters and try again :)");
+            }
+                    
+}
 
+void printPhoneRow(struct Phone p){
+    printf("\n1. Model Name - %s \n2. Price - %d \n3. RAM - %d \n4. Storage - %d \n5. Camera - %d \n6. Battery - %d \n \n", p.name, p.price, p.ram, p.storage, p.camera, p.battery);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
 }
